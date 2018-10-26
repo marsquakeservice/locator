1c2ad312147933dabaef19ac3302e3e4e6ca1c7c
sstaehler <simon.staehler@erdw.ethz.ch>
2018-10-26 22:54:30 +0200

    Version that creates output file as the big singlestationlocator. Not tested with GUI yet, but slightly faster

--- locator/locator.py
+++ locator/locator.py
@@ -10,6 +10,7 @@ from h5py import File
 import glob
 import matplotlib.pyplot as plt
 from obspy import UTCDateTime
+from yaml import load, dump
 
 def load_tt(files, phase_list):
 
@@ -28,33 +29,16 @@ def load_tt(files, phase_list):
             for iphase, phase in enumerate(phase_list):
                 idx = phase_names.index(phase.encode())
                 tt[ifile, :, :, iphase] = f['/body_waves/times'][:, :, idx]
+
+    idx_P = phase_list.index('P')
     tt_P = np.zeros((len(files), ndepth, ndist, 1), dtype='float32')
-    tt_P[:, :, :, 0] = tt[:, :, :, 0]
+    tt_P[:, :, :, 0] = tt[:, :, :, idx_P]
     tt -= tt_P
-    return tt, depths, distances
+    return tt, depths, distances, tt_P
 
 
-if __name__ == '__main__':
-    tt, dep, dis = load_tt(files=glob.glob('tt/mantlecrust_00???.h5'),
-                           phase_list=['P', 'pP', 'S'])
-
-    tt_meas = np.asarray([float(UTCDateTime('2019-01-03T15:09:54.9')),
-                          #float(UTCDateTime('2019-01-03T15:11:47.5')),
-                          float(UTCDateTime('2019-01-03T15:10:07.8')),
-                          float(UTCDateTime('2019-01-03T15:18:34.6'))])
-
-    tt_meas -= tt_meas[0]
-    tt_meas[tt_meas==-1] = 1e5
-    modelling_error = 2
-    sigma = np.asarray([1, 2, 5])
-    sigma += modelling_error
-    misfit = ((tt - tt_meas) / sigma)**2
-    nphase = len(tt_meas)
-    nmodel = tt.shape[0]
-    ndepth = len(dep)
-    ndist = len(dis)
-    p = 1./np.sqrt((2*np.pi)**nphase*np.prod(sigma)) * np.prod(np.exp(-misfit), axis=3)
-
+def plot(p, dis, dep):
+    nmodel, ndepth, ndist = p.shape
     depthdist = np.sum(p, axis=(0)) / nmodel
     plt.contourf(dis, dep, depthdist)
     plt.colorbar()
@@ -63,10 +47,109 @@ if __name__ == '__main__':
     plt.tight_layout()
     plt.savefig('depth_distance.png')
     plt.show()
-
-    plt.plot(np.sum(p, axis=(1,2)) / ndepth / ndist, '.')
+    plt.plot(np.sum(p, axis=(1, 2)) / ndepth / ndist, '.')
     plt.xlabel('Model index')
     plt.ylabel('likelihood')
     plt.tight_layout()
     plt.savefig('model_likelihood.png')
     plt.show()
+
+
+def open_yaml(filename):
+    with open(filename, 'r') as f:
+        input = load(f)
+    return input['phases']
+
+
+def serialize_phases(phases):
+    phase_list = []
+    tt_meas = np.zeros(len(phases))
+    sigma = np.zeros_like(tt_meas)
+
+    for iphase, phase in enumerate(phases):
+        phase_list.append(phase['code'])
+        if phase['code'] in ['P', 'P1', 'PKP']:
+            iref = iphase
+
+        tt_meas[iphase] = float(UTCDateTime(phase['datetime']))
+        sigma[iphase] = phase['uncertainty_upper'] + phase['uncertainty_lower']
+
+    tt_ref = tt_meas[iref]
+    tt_meas -= tt_ref
+
+    return phase_list, tt_meas, sigma, tt_ref
+
+
+def write_result(file_out, p, dep, dis, tt_P):
+    nmodel, ndepth, ndist = p.shape
+    result = dict()
+    p_dist = np.sum(p, axis=(0, 1))
+    p_depth = np.sum(p, axis=(0, 2))
+
+    pdf_depth_sum = []
+    for idepth, d in enumerate(dep):
+        pdf_depth_sum.append([d, p_depth[idepth]])
+    depth_sum = np.sum(dep * p_depth) / np.sum(p_depth)
+
+    pdf_dist_sum = []
+    for idist, d in enumerate(dis):
+        pdf_dist_sum.append([d, p_dist[idist]])
+    dist_sum = np.sum(dis * p_dist) / np.sum(p_dist)
+
+    origin_pdf, origin_times = np.histogram(a=-tt_P.flatten(),
+                                            weights=p.flatten(),
+                                            bins=np.linspace(-600, 0, 100),
+                                            density=True)
+    pdf_origin_sum = []
+    time_bin_mid = (origin_times[0:-1] + origin_times[1:]) / 2.
+    for itime, time in enumerate(time_bin_mid):
+        pdf_origin_sum.append([time,
+                               origin_pdf[itime]])
+    origin_time_sum = UTCDateTime(tt_ref) + np.sum(origin_pdf * time_bin_mid)
+
+    with open(file_out, 'w') as f:
+        write_prob(f, pdf_depth_sum=pdf_depth_sum,
+                   pdf_dist_sum=pdf_dist_sum,
+                   pdf_otime_sum=pdf_origin_sum)
+        write_single(f, depth_sum=depth_sum, dist_sum=dist_sum,
+                     origin_time_sum=origin_time_sum )
+        # §f.write('pdf_depth_sum:\n\n')
+        # §f.write('  probabilities: ')
+        # §print(pdf_depth_sum, file=f)
+
+
+def write_prob(f, **kwargs):
+    for key, value in kwargs.items():
+        f.write('%s: \n' % key)
+        f.write('  probabilities: ')
+        print(value, file=f)
+        f.write('\n\n')
+
+def write_single(f, **kwargs):
+    for key, value in kwargs.items():
+        f.write('%s: %f\n\n' % (key, value))
+
+if __name__ == '__main__':
+    input_file = './data/locator_input.yaml'
+    output_file = './data/locator_output.yaml'
+    phase_list, tt_meas, sigma, tt_ref = serialize_phases(open_yaml(input_file))
+
+    model_path = '../tt/mantlecrust_00???.h5'
+    files = glob.glob(model_path)
+    files.sort()
+    tt, dep, dis, tt_P = load_tt(files=files,
+                                 phase_list=phase_list)
+    tt[tt==-1] = 1e5
+    tt_P[tt_P==-1] = 1e5
+
+    modelling_error = 2
+    misfit = ((tt - tt_meas) / sigma)**2
+    nphase = len(tt_meas)
+    nmodel = tt.shape[0]
+    ndepth = len(dep)
+    ndist = len(dis)
+    p = 1./np.sqrt((2*np.pi)**nphase*np.prod(sigma)) * np.prod(np.exp(-misfit), axis=3)
+
+
+    write_result(file_out=output_file, p=p, dep=dep, dis=dis, tt_P=tt_P)
+    plot(p, dep=dep, dis=dis)
