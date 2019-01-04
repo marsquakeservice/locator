03a3595054c3394a5eb59c23e5ea800d72fbac0e
sstaehler <simon.staehler@erdw.ethz.ch>
2019-01-04 21:30:50 +0100

    Refactor script to create tests

--- locator/data/tests/create_test.py
+++ locator/data/tests/create_test.py
@@ -10,13 +10,15 @@ import obspy
 from obspy.taup import TauPyModel
 import argparse
 
-parser = argparse.ArgumentParser(
-    description='Create test file with surface and body wave traveltimes')
-parser.add_argument('ievent', type=int,
-                    help='event number in surface wave traveltime file')
-parser.add_argument('depth', type=float,
-                   help='event depth in kilometer')
-args = parser.parse_args()
+
+def define_arguments():
+    parser = argparse.ArgumentParser(
+        description='Create test file with surface and body wave traveltimes')
+    parser.add_argument('ievent', type=int,
+                        help='event number in surface wave traveltime file')
+    parser.add_argument('depth', type=float,
+                       help='event depth in kilometer')
+    return parser.parse_args()
 
 
 def create_input(phases, baz):
@@ -35,35 +37,36 @@ def create_input(phases, baz):
             if phase['code'] in ['R1', 'G1']:
                 f.write('    frequency: %s\n' % phase['frequency'])
 
-model = TauPyModel('./locator/data/tests/MSS_ORT/MQSORT_TAY.npz')
 
-ievent = args.ievent
-origin_time = obspy.UTCDateTime('2019-01-01T00:00:00')
-depth = args.depth
-phases = []
-for period in [14., 40., 113.]:
-    surface_wave_times = np.loadtxt('./locator/data/tests/MSS_ORT/ttr_%03d.txt' % period)
-    time = surface_wave_times[ievent]
-    phase = {'code': 'R1',
-             'datetime': '%s' % (origin_time + time[2]),
-             'frequency': 1./period,
-             'sigma': 80.
-             }
-    phases.append(phase)
-baz = time[1]
-dist = time[0]
-
-for phase_name in ['P', 'S', 'PP', 'pP']:
-    arr = model.get_travel_times(source_depth_in_km=depth,
-                                 distance_in_degree=dist,
-                                 phase_list=[phase_name])
-    if len(arr) > 0:
-        phase = {'code': phase_name,
-                 'datetime': '%s' % (origin_time + arr[0].time),
-                 'sigma': 5.
+def create_event(ievent, depth):
+    model = TauPyModel('./locator/data/tests/MSS_ORT/MQSORT_TAY.npz')
+    origin_time = obspy.UTCDateTime('2019-01-01T00:00:00')
+    phases = []
+    for period in [14., 40., 113.]:
+        surface_wave_times = np.loadtxt('./locator/data/tests/MSS_ORT/ttr_%03d.txt' % period)
+        time = surface_wave_times[ievent]
+        phase = {'code': 'R1',
+                 'datetime': '%s' % (origin_time + time[2]),
+                 'frequency': 1. / period,
+                 'sigma': 80.
                  }
         phases.append(phase)
-print('True distance: %5.1f degree' % dist)
-create_input(phases, baz)
+    baz = time[1]
+    dist = time[0]
+    for phase_name in ['P', 'S', 'PP', 'pP']:
+        arr = model.get_travel_times(source_depth_in_km=depth,
+                                     distance_in_degree=dist,
+                                     phase_list=[phase_name])
+        if len(arr) > 0:
+            phase = {'code': phase_name,
+                     'datetime': '%s' % (origin_time + arr[0].time),
+                     'sigma': 5.
+                     }
+            phases.append(phase)
+    print('True distance: %5.1f degree' % dist)
+    create_input(phases, baz)
 
 
+if __name__ == '__main__':
+    args = define_arguments()
+    create_event(args.ievent, args.depth)
